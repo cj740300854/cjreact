aba62956a7bbfb0784a33394fe1de55582b374fe
cj740300854 <chenjian@ayibang.com>
2016-05-10 18:56:09 +0800

    提交

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("babel");
+var sum = function sum(num1, num2) {
+  return num1 + num2;
+};
+var x = sum(1, 2);
+console.log(x);
+console.log(111);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-var a= 0;
+console.log("babel");
+const sum = (num1,num2) => num1+ num2;
+let x = sum(1,2);
+console.log(x);
+console.log(111)
